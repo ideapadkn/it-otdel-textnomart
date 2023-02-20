4519893112d7e760a1fc6cf431b6993ecc0b5b07
Kamron or Камрон <kamrush6429@gmail.com>
2023-02-20 14:51:29 +0500

    add back-end employees

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    <h1 class="gl-title">Структрура IT подразделения Texnomart</h1>
+    <div 
+      class="content" 
+      v-for="content in contents" 
+      :key="content.id"
+    >
+      <img src="@/assets/ShuhratMengaliev.png" alt="img">
+      <h4 class="title">{{ content.title }}</h4>
+      <p class="body">{{ content.body }}</p>
+    </div>
+    <back-end />
+  </div>
 </template>
 
-<style>
+<script>
+import backEnd from './components/back-end/back-end.vue';
+export default {
+  components: {
+    backEnd
+  },
+  data() {
+    return {
+      contents: [
+        {id: 1, title: 'Шухрат Менгалиев', body: 'ИТ Директор'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  padding: 80px 140px;
 }
-
-nav {
-  padding: 30px;
+.container {
+  max-width: 1400px;
+  margin: 0 auto;
+  .gl-title {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 40px;
+    line-height: 140%;
+    color: #333333;
+    margin-bottom: 80px;
+  }
+  .content {
+    border: 2px solid #fbc100;
+    width: 210px;
+    height: 198px;
+    border-radius: 32px;
+    padding: 24px 23px;
+    text-align: center;
+  }
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.title {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 24px;
+  color: #333333;
+  margin: 20px 0 6px 0;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.body {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #767676;
 }
 </style>
---
+++ src/components/back-end/back-end.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="back-end">
+    <h2 style="margin: 80px 0 16px 0;">Бекенд разработчики</h2>
+    <div class="back-content">
+      <div 
+        class="inner-content" 
+        v-for="content in backContent" 
+        :key="content.id"
+      >
+        <div>{{ content.img }}</div>
+        <h4 class="title">{{ content.title }}</h4>
+        <p class="body">{{ content.body }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        backContent: [
+          {id: 1, img: 'img', title: 'Исхокжон', body: 'Team Lead Backend'},
+          {id: 2, img: 'img', title: 'Жавохир', body: 'Бекенд разработчик'},
+          {id: 3, img: 'img', title: 'Сарвар ', body: 'Бекенд разработчик'},
+          {id: 4, img: 'img', title: 'Шукурулло', body: 'Бекенд разработчик'},
+          {id: 5, img: 'img', title: 'Жавохир', body: 'Бекенд разработчик'},
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.back-end {
+  margin-left: 92px;
+  .back-content {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 10px;
+    gap: 16px;
+    width: 784px;
+    height: 210px;
+    background: #FAFAFA;
+    border: 1px dashed #D5D8DF;
+    border-radius: 28px;
+    .inner-content {
+      padding: 16px 12px;
+      text-align: center;
+      width: 140px;
+      height: 188px;
+      background: #FFFFFF;
+      box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08);
+      border-radius: 24px;
+    }
+  }
+}
+</style>
